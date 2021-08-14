1c14ac792c64d13793d33210f51c88818a36f38e
thaochang <88922939+thaochang@users.noreply.github.com>
2021-08-14 14:13:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    Mà sao chị cứ đi nhầm đường
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em<33',
+    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là yêu em<33',
     btnNo: 'khum,em',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chi lại yêu em <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
     btnReply: 'Gửi cho em <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: '',
     mess: 'em biết mà 🥰. Yêu chi nhiều nhiều 😘😘',
     messDesc: 'het dich minh di choi nhe.',
     btnAccept: 'Okiiiii lun <3',
